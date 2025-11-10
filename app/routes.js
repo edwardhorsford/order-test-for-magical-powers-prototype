33159858935425f9bac117dbdec61c41cb641e8f
Ed Horsford <mail@edwardhorsford.com>
2025-11-10 15:24:13 +0000

    Add routing for ineligible

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,22 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Called when answering question about whether NHS number is known
+router.post("/magical-powers-answer", (req, res) => {
+  // Make a variable and give it the value from 'nhsNumberKnown'
+  const hasSymptoms = req.session.data.hasSymptoms;
+
+  // Check whether the variable matches a condition
+  if (hasSymptoms === "Yes") {
+    // Send user to a page where they'll enter their NHS number
+    res.redirect("/symptoms");
+  } else if (hasSymptoms === "No") {
+    // Send user to a page where they can find their NHS number
+    res.redirect("/ineligible");
+  } else {
+    // Send user back to the question page
+    res.redirect("/magical-powers");
+  }
+});
+
 module.exports = router
